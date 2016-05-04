f64123bc3f5542c07ac21bdfe226e5c874a29eef
mlantin <maria.lantin@gmail.com>
2016-05-04 15:54:42 -0700

    added support for hydra in protobuf. But...but...the socket.io client library is buggy I think. it keeps disconnecting after a set amount of time. It's very hard to debug why. A cursory search on the web seems to indicate that in the 3.5 version of the library it creates too many threads. This would be hundreds of threads for me as I'm sending about 30 updates per second. No wonder it crashes. But it's interesting that it disconnects and doesn't try to reconnect. Maybe I'll post a message. Thought they don't officially support Unity.

--- Assets/SocketDispatch.cs
+++ Assets/SocketDispatch.cs
@@ -7,7 +7,7 @@ public class SocketDispatch : MonoBehaviour {
 	public string url = "http://127.0.0.1:4567";
 	public GameObject testobj = null;
 	Vector3 newpos = new Vector3();
-
+	Quaternion newrot = new Quaternion();
 	private Socket socket;
 
 	// Use this for initialization
@@ -18,19 +18,31 @@ public class SocketDispatch : MonoBehaviour {
 				Debug.Log("Connected!");
 
 			});
-		socket.On ("mocap", (data) => {
-			Google.Protobuf.VRCom.Mocap msg = Google.Protobuf.VRCom.Mocap.Parser.ParseFrom((System.Byte[])data);
-			newpos.Set(msg.Pos.X/100, msg.Pos.Y/100, msg.Pos.Z/100);
+		socket.On (Socket.EVENT_ERROR, () => {
+			Debug.Log ("Event Error!");
+		});
+
+		socket.On (Socket.EVENT_DISCONNECT, () => {
+			Debug.Log ("Disconnecting!");
+		});
+		socket.On (Socket.EVENT_RECONNECTING, () => {
+			Debug.Log ("Reconnecting!");
+		});
+		socket.On ("hydra", (data) => {
+			Google.Protobuf.VRCom.Hydra msg = Google.Protobuf.VRCom.Hydra.Parser.ParseFrom((System.Byte[])data);
+			if (msg.CtrlNum == 0)
+				newrot.Set(msg.Rot.X, msg.Rot.Y, msg.Rot.Z, msg.Rot.Z);
 
 		});
 	}
 
 	// Update is called once per frame
 	void Update () {
-		testobj.transform.position = newpos;
+		testobj.transform.rotation = newrot;
 	}
 
 	void OnApplicationQuit() {
+		Debug.Log ("quitting");
 		socket.Close ();
 	}
 }
